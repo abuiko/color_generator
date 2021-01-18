25460a160514ad282d33c2d4f271087e3ad4dd8c
Alena Buiko <abuiko.ma@gmail.com>
2021-01-17 20:27:05 -0500

    Style input range bar

--- app.js
+++ app.js
@@ -83,8 +83,43 @@ function randomColors() {
     const hexText = color.querySelector(".hex-line");
     hexText.innerText = randomColor;
     checkTextContrast(randomColor, hexText);
+
+    // new code starts
+    const colorDiv = chroma(randomColor);
+    const sliders = color.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(colorDiv, hue, brightness, saturation);
+
+    // new code ends
   });
 }
+
+// new code starts
+function colorizeSliders(colorDiv, hue, brightness, saturation) {
+  // scale saturation
+  const noSat = colorDiv.set("hsl.s", 0);
+  const fullSat = colorDiv.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, colorDiv, fullSat]);
+  //   scale brightness
+  const midBright = colorDiv.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  //  update input colors
+
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )}, ${scaleBright(0.5)},${scaleBright(1)})`;
+
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(255, 0, 0), rgb(255,255 ,0),rgb(0, 255, 0),rgb(0, 255, 255),rgb(0,0,255),rgb(255,0,255),rgb(255,0,0))`;
+}
+// new code ends
 
 function generateHex() {
   const colorHex = chroma.random();
