81e8cc4d6f1aadb9307da7449386e4c16b768a58
Alena Buiko <abuiko.ma@gmail.com>
2021-05-14 20:10:58 -0400

    fix library popup

--- dist/js/app.js
+++ dist/js/app.js
@@ -410,3 +410,117 @@ getLocal();
 init();
 
 randomColors();
+
+
+
+
+//   // generate object
+//   let paletteNr = savedPalettes.length;
+//   const paletteObj = { name, colors, nr: paletteNr };
+//   savedPalettes.push(paletteObj);
+//   // save to local storage
+//   saveToLocal(paletteObj);
+//   saveInput.value = "";
+//   // generate palette for library
+//   const palette = document.createElement("div");
+//   palette.classList.add("custom-palette");
+//   const title = document.createElement("h4");
+//   title.innerText = paletteObj.name;
+//   const preview = document.createElement("div");
+//   preview.classList.add("small-preview");
+//   paletteObj.colors.forEach(smallColor => {
+//     const smallDiv = document.createElement("div");
+//     smallDiv.style.backgroundColor = smallColor;
+//     preview.appendChild(smallDiv);
+//   });
+//   const paletteBtn = document.createElement("button");
+//   paletteBtn.classList.add("pick-palette-btn");
+//   paletteBtn.classList.add(paletteObj.nr);
+//   paletteBtn.innerText = "Select";
+//   // attach events to the button
+//   paletteBtn.addEventListener("click", e => {
+//     closeLibrary();
+//     const paletteIndex = e.target.classList[1];
+//     initialColors = [];
+//     savedPalettes[paletteIndex].colors.forEach((color, index) => {
+//       initialColors.push(color);
+//       colorDivs[index].style.backgroundColor = color;
+//       const text = colorDivs[index].children[0];
+//       checkTextContrast(color, text);
+//       updateTextUI(index);
+//     });
+//     resetInputs();
+//   });
+//   // append to library
+//   palette.appendChild(title);
+//   palette.appendChild(preview);
+//   palette.appendChild(paletteBtn);
+//   libraryContainer.children[0].appendChild(palette);
+// }
+// function saveToLocal(paletteObj) {
+//   let localPalettes;
+//   if (localStorage.getItem("palettes") === null) {
+//     localPalettes = [];
+//   } else {
+//     localPalettes = JSON.parse(localStorage.getItem("palettes"));
+//   }
+//   localPalettes.push(paletteObj);
+//   localStorage.setItem("palettes", JSON.stringify(localPalettes));
+// }
+// function openLibrary() {
+//   const popup = libraryContainer.children[0];
+//   libraryContainer.classList.add("active");
+//   popup.classList.add("active");
+// }
+// function closeLibrary() {
+//   const popup = libraryContainer.children[0];
+//   libraryContainer.classList.remove("active");
+//   popup.classList.remove("active");
+// }
+// function getLocal() {
+//   if (localStorage.getItem("palettes") === null) {
+//     localPalette = [];
+//   } else {
+//     const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+//     paletteObjects.forEach(paletteObj => {
+//       // generate palette for library
+//       const palette = document.createElement("div");
+//       palette.classList.add("custom-palette");
+//       const title = document.createElement("h4");
+//       title.innerText = paletteObj.name;
+//       const preview = document.createElement("div");
+//       preview.classList.add("small-preview");
+//       paletteObj.colors.forEach(smallColor => {
+//         const smallDiv = document.createElement("div");
+//         smallDiv.style.backgroundColor = smallColor;
+//         preview.appendChild(smallDiv);
+//       });
+//       const paletteBtn = document.createElement("button");
+//       paletteBtn.classList.add("pick-palette-btn");
+//       paletteBtn.classList.add(paletteObj.nr);
+//       paletteBtn.innerText = "Select";
+//       // attach events to the button
+//       paletteBtn.addEventListener("click", e => {
+//         closeLibrary();
+//         const paletteIndex = e.target.classList[1];
+//         initialColors = [];
+//         paletteObjects[paletteIndex].colors.forEach((color, index) => {
+//           initialColors.push(color);
+//           colorDivs[index].style.backgroundColor = color;
+//           const text = colorDivs[index].children[0];
+//           checkTextContrast(color, text);
+//           updateTextUI(index);
+//         });
+//         resetInputs();
+//       });
+//       // append to library
+//       palette.appendChild(title);
+//       palette.appendChild(preview);
+//       palette.appendChild(paletteBtn);
+//       libraryContainer.children[0].appendChild(palette);
+//     });
+//   }
+// }
+
+// getLocal();
+// randomColors();
